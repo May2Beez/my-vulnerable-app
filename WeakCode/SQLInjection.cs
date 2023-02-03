d21252e59044eb2e0d5cd3b1f007a1e8aff7555b
Jk1best <66845678+Jk1best@users.noreply.github.com>
2023-02-03 14:15:43 +0100

    Initial commit

---
+++ WeakCode/SQLInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vulnerable_app.WeakCode
+{
+    internal class SQLInjection
+    {
+    }
+}
---
+++ WeakCode/WeakHash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SASTTest
+{
+    class WeakHash
+    {
+        static void Main(string[] args)
+        {
+            string input = "SAST_Test";
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            SHA1 sha1 = SHA1.Create();
+            byte[] hash = sha1.ComputeHash(inputBytes);
+
+            Console.WriteLine(BitConverter.ToString(hash));
+        }
+    }
+}
